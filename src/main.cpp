0d819a2e815d709e0cd8b90c673a04f3f8a54251
Luka Matic <maticluka888@gmail.com>
2024-03-25 20:25:39 +0100

    added a backpack model

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <learnopengl/filesystem.h>
+#include <learnopengl/shader_m.h>
 #include <learnopengl/camera.h>
+#include <learnopengl/model.h>
 
 #include <iostream>
 
@@ -37,7 +40,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "project", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "project", nullptr, nullptr);
     if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -51,11 +54,24 @@ int main() {
 
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    stbi_set_flip_vertically_on_load(true);
+
+    glEnable(GL_DEPTH_TEST);
+
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // build and compile shaders
+    // -------------------------
+    Shader baseballShader("resources/shaders/backpack.vs", "resources/shaders/backpack.fs");
+
+    // load models
+    // -----------
+    Model baseballModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
 
     while (!glfwWindowShouldClose(window)) {
         // per-frame time logic
@@ -67,7 +83,23 @@ int main() {
         processInput(window);
 
         glClearColor(0.0f, 0.2f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // don't forget to enable shader before setting uniforms
+        baseballShader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        baseballShader.setMat4("projection", projection);
+        baseballShader.setMat4("view", view);
+
+        // render the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        baseballShader.setMat4("model", model);
+        baseballModel.Draw(baseballShader);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
